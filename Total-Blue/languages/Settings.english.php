39bed00fc4ca13625c8646e5233fb2204f800a25
Trax2k <jplant01@gmail.com>
2016-04-19 11:51:03 +0100

    N/A

---
+++ Total-Blue/languages/Settings.english.php
@@ -0,0 +1,11 @@
+<?php
+// Version: 2.0 RC5; Settings
+
+// Important! Before editing these language files please read the text at the top of index.english.php.
+
+global $settings;
+
+$txt['theme_thumbnail_href'] = $settings['images_url'] . '/thumbnail.gif';
+$txt['theme_description'] = 'Very clean and modern theme.<br /><br />Total Blue by <a href="http://www.smfhost.xyz" target="_blank">SMF Host</a>.';
+
+?>
