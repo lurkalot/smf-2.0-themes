a44530811c056118a78c53c5e52a8aed7e58be87
Trax2k <jplant01@gmail.com>
2016-04-19 12:26:13 +0100

    N/A

---
+++ GrayStyle/languages/ThemeStrings.english.php
@@ -0,0 +1,11 @@
+<?php
+// Version: 2.0; ThemeStrings
+
+$txt['facebook_url'] = 'Facebook URL';
+$txt['twitter_url'] = 'Twitter URL';
+$txt['youtube_url'] = 'Youtube URL';
+$txt['google_url'] = 'Google+ URL';
+$txt['rss_url'] = 'RSS URL';
+$txt['unread'] = 'Show unread posts';
+$txt['unread_replies'] = 'Show new replies';
+?>
