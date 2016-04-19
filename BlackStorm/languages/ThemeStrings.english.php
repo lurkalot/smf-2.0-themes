87a30ae631a5f27b9738aeb199f08bc6014005c0
Trax2k <jplant01@gmail.com>
2016-04-19 12:07:42 +0100

    N/A

---
+++ BlackStorm/languages/ThemeStrings.english.php
@@ -0,0 +1,14 @@
+<?php
+// Version: 2.0; black storm by trax
+global $context, $scripturl;
+
+$txt['enable_bookmarks'] = 'Enable Social Icons';
+$txt['enable_twitter'] = 'Twitter';
+$txt['twitter_desc'] = 'Put your twitter url here.';
+$txt['enable_facebook'] = 'Facebook';
+$txt['facebook_desc'] = 'Put your facebook url here.';
+$txt['enable_youtube'] = 'Youtube';
+$txt['youtube_desc'] = 'Put your youtube url here.';
+$txt['enable_rss'] = 'RSS';
+$txt['rss_desc'] = 'Put your RSS url here.';
+?>
