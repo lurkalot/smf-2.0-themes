75b6d272968fffb6dd9014d34c97de2e414eae9d
Trax2k <jplant01@gmail.com>
2016-04-19 11:58:57 +0100

    N/A

---
+++ ProRedV1/languages/Settings.english.php
@@ -0,0 +1,11 @@
+<?php
+// Version: 2.0; Settings
+
+// Important! Before editing these language files please read the text at the top of index.english.php.
+
+global $settings;
+
+$txt['theme_thumbnail_href'] = $settings['images_url'] . '/thumbnail.gif';
+$txt['theme_description'] = 'Very clean and modern smf theme.<br /><br />Pro Red by <a href="http://www.smfhost.xyz" target="_blank">Trax</a>';
+
+?>
